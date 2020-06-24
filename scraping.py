529cd6bde4c861a84f9e73c9adb2be805fa368b8
Anand Tyagi <ananddtyagi@yahoo.com>
2020-06-24 15:47:15 -0400

    completed entry of glossary data into the final file, now onto checking for errors

--- scraping.py
+++ scraping.py
@@ -1,18 +1,36 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import sys
-
+import json
 
 url = sys.argv[1]
 # url = https://en.wikipedia.org/wiki/Glossary_of_calculus
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, "lxml")
 
-print(soup.findAll('dt', class_='glossary')[0].find(text=True))
-print(''.join(soup.findAll('dd', class_='glossary')[0].findAll(text=True)))
 
 terms = soup.findAll('dt', class_='glossary')
 definitions = soup.findAll('dd', class_='glossary')
 
 assert(len(terms) == len(definitions))
+
+final_list = []
+
+for i in range(len(terms)):
+
+    obj = {
+        "standard" : {
+            'term': terms[i].text,
+            'definition': definitions[i].text
+            },
+        "system": {
+            'term': '',
+            'definition': ''
+            }
+    }
+
+    final_list.append(obj)
+
+with open(sys.argv[2] + '.txt', 'w') as outfile:
+    json.dump(final_list, outfile)
 
