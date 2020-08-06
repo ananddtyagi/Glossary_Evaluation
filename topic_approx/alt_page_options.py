315521ad5d215a1a4ecc7818b11eded8b01eab7f
Anand Tyagi <ananddtyagi@yahoo.com>
2020-08-06 15:34:41 -0400

    finished skeleton for supertopic selection.

--- topic_approx/alt_page_options.py
+++ topic_approx/alt_page_options.py
@@ -1,5 +1,27 @@
 from didyoumean import didyoumean
 
 spell = "Poshgresql"
-correctSpell = didyoumean.didYouMean(spell, ['hello', 'posgres', 'postgres', 'postgressql'])
-print (correctSpell)
+pages = ['hello', 'posgres', 'postgres', 'postgressql']
+correctSpell = didyoumean.didYouMean(spell, pages)
+
+while (input('would you like to search for a page?\n') == 'y'):
+
+    query = input('enter the subclass\n')
+
+    match = didyoumean.didYouMean(query, pages)
+    if match == None:
+        print('No match was found\n')
+    elif match in pages:
+        continue;
+    else:
+        resp = input('Did you mean ' +  match + '?\n')
+        while resp == 'n':
+            pages.remove(match)
+            match = didyoumean.didYouMean(query, pages)
+            if match == None:
+                print('No other matches were found\n')
+                break;
+            else:
+                resp = input('Did you mean ', match, '?\n')
+
+
