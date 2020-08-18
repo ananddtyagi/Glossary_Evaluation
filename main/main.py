8efa548e913057466e0297ccca52eec4727917fc
Anand Tyagi <ananddtyagi@yahoo.com>
2020-08-18 17:04:11 -0400

    outline for final structure

---
+++ main/main.py
@@ -0,0 +1,8 @@
+#get subject from user
+#use wikipedia package to find the closest valid page
+#confirm page
+#take subclass from that page
+#use that subclass to perform get_superclass
+#have user choose from the multiple superclass
+#run equal split of articles with each different superclass
+#modify shell file summary_from_2_terms to handle multiple superclasses
